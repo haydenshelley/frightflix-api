fdb970752b580bc232f42c0244d023b2a3ca7896
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-16 12:01:50 -0700

    generate movie model and add not_so_scary_movies seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+not_so_scary_movies = CSV.read('not_so_scary.csv')
+p not_so_scary_movies[0][0]
+p not_so_scary_movies[0][1]
+p not_so_scary_movies[0][2]
+
+
+not_so_scary_movies.each do |movie|
+  Movie.create!(
+    title: movie[0],
+    year: movie[1],
+    description: movie[2],
+    category_id: 1
+  )
+end
