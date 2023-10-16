cc90dcf5e5690c5d78f58146232481c6b497e695
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-16 12:20:23 -0700

    create seeds file for data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,29 @@
-not_so_scary_movies = CSV.read('not_so_scary.csv')
-p not_so_scary_movies[0][0]
-p not_so_scary_movies[0][1]
-p not_so_scary_movies[0][2]
+# not_so_scary_movies = CSV.read('not_so_scary.csv')
+# not_so_scary_movies.each do |movie|
+#   Movie.create!(
+#     title: movie[0],
+#     year: movie[1],
+#     description: movie[2],
+#     category_id: 1
+#   )
+# end
 
+# scary_movies = CSV.read('scary.csv')
+# scary_movies.each do |movie|
+#   Movie.create!(
+#     title: movie[0],
+#     year: movie[1],
+#     description: movie[2],
+#     category_id: 2
+#   )
+# end
 
-not_so_scary_movies.each do |movie|
-  Movie.create!(
-    title: movie[0],
-    year: movie[1],
-    description: movie[2],
-    category_id: 1
-  )
-end
+# extremely_scary_movies = CSV.read('extremely_scary.csv')
+# extremely_scary_movies.each do |movie|
+#   Movie.create!(
+#     title: movie[0],
+#     year: movie[1],
+#     description: movie[2],
+#     category_id: 3
+#   )
+# end
