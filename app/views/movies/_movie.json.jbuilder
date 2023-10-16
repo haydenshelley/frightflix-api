aeeba304000265c58e2dd4aa8dce8c2fa79ef20b
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-16 12:28:54 -0700

    movies index and show routes

---
+++ app/views/movies/_movie.json.jbuilder
@@ -0,0 +1,5 @@
+json.id movie.id
+json.title movie.title
+json.year movie.year
+json.description movie.description
+json.category_id movie.category_id
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "/movies" => "movies#index"
+  get "/movies/:id" => "movies#show"
+
+  # user signup and login actions
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
 end
